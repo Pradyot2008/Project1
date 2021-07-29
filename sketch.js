616de8a82a1436efed4ef53d535d669c69b695bb
Pradyot2008 <87645837+Pradyot2008@users.noreply.github.com>
2021-07-29 15:54:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+
+
+function setup() {
+
+}
+
+function draw() 
+{
+  background("Yellow");
+  if(keyDown("LEFT")){
+    background("Blue")
+  } 
+   if(keyDown("RIGHT")){
+    background("purple")
+  }  
+  if(keyDown("UP")){
+    background("red")
+  } 
+   if(keyDown("DOWN")){
+    background("green")
+  }
+}
